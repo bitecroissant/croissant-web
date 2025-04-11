e9bdb8e6af657d6eced5321bdf1c7f0bf90bd948
bitecroissant <bitecroissant@hotmail.com>
2025-04-12 00:12:52 +0800

    👌 封装 Icon.tsx 组件

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import s from './Profile.module.css'
+import { Icon } from "./Icon"
 
 export const Profile: React.FC = () => {
   // const { isLoading, isAuthenticated, user, getAccessTokenSilently } = useAuth0()
 
   // if (isLoading) {
   if (true) {
-    return <div className={s.loadingText}> Loading </div>
+    return <Icon name="loading" />
   }
 
   return (
