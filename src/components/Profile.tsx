ed28ff6c046fe102d2c5dceaf6562106b681e8f7
bitecroissant <bitecroissant@hotmail.com>
2025-04-11 21:01:22 +0800

    🥻 安装 sass，试用 css module

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react"
+import s from './Profile.module.css'
 
 export const Profile: React.FC = () => {
   // const { isLoading, isAuthenticated, user, getAccessTokenSilently } = useAuth0()
 
   // if (isLoading) {
   if (true) {
-    return <div> Loading </div>
+    return <div className={s.loadingText}> Loading </div>
   }
 
   return (
